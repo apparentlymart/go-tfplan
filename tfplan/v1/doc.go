162d18c560a234ded0e2c4ebd1ad9dab65a4b6a8
Martin Atkins <mart@degeneration.co.uk>
2017-11-05 18:19:39 -0800

    More elaborate docs for the version packages.

--- tfplan/v1/doc.go
+++ tfplan/v1/doc.go
@@ -1,2 +1,5 @@
 // Package v1 contains types used to represent Terraform plan version 1.
+//
+// Types in this package were copied -- mostly mechanically -- from Terraform
+// itself. Specifically they were copied from v0.9.10.
 package v1
--- tfplan/v2/doc.go
+++ tfplan/v2/doc.go
@@ -1,2 +1,5 @@
 // Package v2 contains types used to represent Terraform plan version 2.
+//
+// Types in this package were copied -- mostly mechanically -- from Terraform
+// itself. Specifically they were copied from v0.10.8.
 package v2
